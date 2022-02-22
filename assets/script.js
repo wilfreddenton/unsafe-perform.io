dc0fa0e281376055c8a4ec5e8fc1e1fc3980ac2a
wilfreddenton <dentonw3@gmail.com>
2022-02-21 23:33:28 -0500

    third pass; bug fixes

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 function fixFootnoteHrefs() {
 	document.querySelectorAll(".footnote-reference a").forEach(e => {
-		e.href = window.location + e.hash
+		e.href = window.location.pathname + e.hash
 	})
 }
 
